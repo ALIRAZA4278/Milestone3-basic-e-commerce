11a58a3940871f92a4e003b63c23021f41eecd15
Ali Raza <156216939+ALIRAZA4278@users.noreply.github.com>
2025-01-22 18:36:31 +0500

    Furniture Ecommerce

--- src/app/Shop/page.tsx
+++ src/app/Shop/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import ShopList from "../Components/ShopList";  
+import ShopList from "../Components/ShopList";
 import DeliveryPage from "../Components/DeliveryPage";
 import Breadcrumb from "../Components/Breadcrumb";
 import Filter from "../Components/Filter";
@@ -8,36 +8,34 @@ import { Suspense } from "react";
 const Shoppage = () => {
     return (
         <Suspense>
-      
-    
-        <div className="bg-white">
-            {/* CAMPAIGN */}
-            <div
-                className="relative bg-cover bg-center h-[calc(40vh-80px)]"
-                style={{
-                    backgroundImage: "url('/ShopBackgroud.png')",
-                }}
-            >
-                {/* Overlay */}
-                <div className="absolute inset-0 bg-opacity-50"></div>
-                {/* Content */}
-                <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-black px-4">
-                    <Image src="/ShopLogo.png" alt="Logo" width={100} height={100} />
-                    <p className="text-4xl md:text-5xl font-normal mb-2">Shop</p>
 
-                    <Breadcrumb />
+
+            <div className="bg-white">
+                {/* CAMPAIGN */}
+                <div
+                    className="relative bg-cover bg-center h-[calc(40vh-80px)]"
+                    style={{
+                        backgroundImage: "url('/ShopBackgroud.png')",
+                    }}
+                >
+                    {/* Overlay */}
+                    <div className="absolute inset-0 bg-opacity-50"></div>
+                    {/* Content */}
+                    <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-black px-4">
+                        <Image src="/ShopLogo.png" alt="Logo" width={100} height={100} />
+                        <p className="text-4xl md:text-5xl font-normal mb-2">Shop</p>
+
+                        <Breadcrumb />
+                    </div>
+                    <Filter />
                 </div>
-                <Filter />
-            </div>
 
 
-            <Suspense>
                 <ShopList />
-            </Suspense>
 
-            <DeliveryPage />
-        </div>
-            </Suspense>
+                <DeliveryPage />
+            </div>
+        </Suspense>
     );
 };
 
